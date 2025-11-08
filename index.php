f0f64c73dd01d9f4fe7d708c4e074109da0fc3fa
Syed Samad <syedsamad313@gmail.com>
2025-11-08 20:22:54 +0530

    Revised Design

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MyPOS – Smart Point of Sale System</title>
+    <title>MyPOS – Smart Point of Sale System - RDBMS</title>
     <style>
         :root {
             --primary: #3B82F6;
